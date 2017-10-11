778e259c94f6a52c2f971da1b439b036d581cd64
oleg <oleggudymenko96@gmail.com>
2017-10-11 20:34:51 +0300

    responsive nav script, inline styles script, complete responsive constr

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+// responsiove nav header
+$('.header__bot .hamburger').click(function(){
+    $('.header__bot').toggleClass('active');
+    $('div.hamburger').toggleClass('active');
+    $('#nav').toggleClass('active');
+    if($(window).width() < 768) {
+        $('#main').toggleClass('hidden');
+    }
+})
+// responsiove nav header
+
+//constructor inline-styles
+function customCss(){
+  $('#design-cart .design-number-input').css('width','111px');
+  $('#step-0.popover').css('display','none');
+  $('#design-action-rensa').text('');
+}
+customCss()
